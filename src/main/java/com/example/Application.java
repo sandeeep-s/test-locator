065b1d51f25d0984b28c072afcf622a65e75ea6b
Sandeep <urssandy84@gmail.com>
2016-10-12 00:01:08 +0530

    Added cors headers

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,13 +6,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-import com.example.model.Item;
 import com.example.model.ItemRepository;
-import com.example.model.ItemSection;
 import com.example.model.ItemSectionRepository;
-import com.example.model.Section;
-import com.example.model.Store;
 import com.example.model.StoreRepository;
 
 @SpringBootApplication
@@ -29,7 +28,23 @@ public class Application {
 	public CommandLineRunner demo(ItemRepository itemRepository, StoreRepository storeRepository,
 			ItemSectionRepository itemSectionRepository) {
 		return (args) -> {
-			
+
 		};
 	}
+
+	
+	//The below bean is added to enable access to service from angular app. Otherwise it is blocked due to CORS
+	@Bean
+	public CorsFilter corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true); // you USUALLY want this
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
 }
--- src/test/curl_rest_create_data.sh
+++ src/test/curl_rest_create_data.sh
@@ -1,12 +1,12 @@
-curl -v -X POST  -H "Content-Type:application/json" -d '{"name":"postedStore"}' http://localhost:8080/stores
+curl -v -X POST  -H "Content-Type:application/json" -d '{"name":"postedStore"}' http://localhost:8090/stores
 
-curl -v -X POST  -H "Content-Type:application/json" -d '{"name":"postedSection", "store":"http://localhost:8080/stores/1"}' http://localhost:8080/sections
+curl -v -X POST  -H "Content-Type:application/json" -d '{"name":"postedSection", "store":"http://localhost:8090/stores/1"}' http://localhost:8090/sections
 
-curl -v -X POST  -H "Content-Type:application/json" -d '{"name":"postedItem"}' http://localhost:8080/items
+curl -v -X POST  -H "Content-Type:application/json" -d '{"name":"postedItem"}' http://localhost:8090/items
 
-curl -v -X POST  -H "Content-Type:application/json" -d '{"name":"postedSection", "item":"http://localhost:8080/items/1","section":"http://localhost:8080/sections/1"}' http://localhost:8080/itemSections
+curl -v -X POST  -H "Content-Type:application/json" -d '{"name":"postedSection", "item":"http://localhost:8090/items/1","section":"http://localhost:8090/sections/1"}' http://localhost:8090/itemSections
 
-curl -v -X PATCH  -H "Content-Type:application/json" -d '{"code":"postedItemCode"}' http://localhost:8080/items/1
+curl -v -X PATCH  -H "Content-Type:application/json" -d '{"code":"postedItemCode"}' http://localhost:8090/items/1
 
 
-#curl -v -X PATCH -H "Content-Type:application/patch+json" -d '[{"op":"add", "path":"/sections/0", "value":{"name":"S2"}}]' http://localhost:8080/stores/1
+#curl -v -X PATCH -H "Content-Type:application/patch+json" -d '[{"op":"add", "path":"/sections/0", "value":{"name":"S2"}}]' http://localhost:8090/stores/1
